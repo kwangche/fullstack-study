c7b41a9726b7430e6c04fa32734f7857532e403b
kwangche <163306559+kwangche@users.noreply.github.com>
2025-08-06 16:35:44 +0900

    리액트 실습문제 추가3

---
+++ REACT/foodit/src/components/FoodForm.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import FileInput from './FileInput';
+
+function sanitize(type, value) {
+  switch (type) {
+    case 'number':
+      return Number(value) || 0;
+
+    default:
+      return value;
+  }
+}
+
+function FoodForm() {
+  const [values, setValues] = useState({
+    imgFile: null,
+    title: '',
+    calorie: 0,
+    content: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+    handleChange(name, sanitize(type, value));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FileInput
+        name="imgFile"
+        value={values.imgFile}
+        onChange={handleChange}
+      />
+      <input name="title" value={values.title} onChange={handleInputChange} />
+      <input
+        type="number"
+        name="calorie"
+        value={values.calorie}
+        onChange={handleInputChange}
+      />
+      <input
+        name="content"
+        value={values.content}
+        onChange={handleInputChange}
+      />
+      <button type="submit">확인</button>
+    </form>
+  );
+}
+
+export default FoodForm;
